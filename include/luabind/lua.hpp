49f8a82e7d3c7f034b14eaa89a5817f842bc5b20
Sargis Khachatryan <sargis.khachatryan@picsart.com>
2022-12-22 11:50:47 +0400

    add cmake flags to control lua include and extern c

--- include/luabind/lua.hpp
+++ include/luabind/lua.hpp
@@ -1,10 +1,20 @@
 #ifndef LUABIND_LUA_HPP
 #define LUABIND_LUA_HPP
 
+#if INCLUDE_LUA_LIB
+
+#if INCLUDE_LUA_LIB_WITH_EXTERN_C
 extern "C" {
+#endif // INCLUDE_LUA_LIB_WITH_EXTERN_C
+
 #include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
+
+#if INCLUDE_LUA_LIB_WITH_EXTERN_C
 }
+#endif // INCLUDE_LUA_LIB_WITH_EXTERN_C
+
+#endif // INCLUDE_LUA_LIB
 
 #endif // LUABIND_LUA_HPP
